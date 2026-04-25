c61b16f7f8ebe1b38f44227c89ea29d48e776930
Rian Britto Pereira <contatorianbritto@gmail.com>
2026-04-25 19:28:05 -0300

    feat: melhorias na api
    -> Agora é possível editar dados de uma task
    -> Agora é possível deletar uma task

--- src/main/java/com/rianb/todolist/controller/TaskController.java
+++ src/main/java/com/rianb/todolist/controller/TaskController.java
@@ -3,8 +3,11 @@ package com.rianb.todolist.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +29,20 @@ public class TaskController {
     @GetMapping
     public List<Task> listar() {
         return repository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Task atualizar(@PathVariable Long id, @RequestBody Task novaTask) {
+        Task task = repository.findById(id).orElseThrow();
+
+        task.setTitle(novaTask.getTitle());
+        task.setConcluida(novaTask.isConcluida());
+
+        return repository.save(task);
     }
 }
